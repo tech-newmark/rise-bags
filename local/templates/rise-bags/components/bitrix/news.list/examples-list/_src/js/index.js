8c58adb2ad5e619df921ce2ed28663728d49027f
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-05-22 17:19:25 +0300

    Исправляю ошибки в скрипте в примерах

--- local/templates/rise-bags/components/bitrix/news.list/examples-list/_src/js/index.js
+++ local/templates/rise-bags/components/bitrix/news.list/examples-list/_src/js/index.js
@@ -1,40 +1,40 @@
 BX.ready(function () {
-  const slider = document.querySelector(".examples-slider");
+	const slider = document.querySelector(".examples-slider");
 
-  console.log(slider);
+	console.log(slider);
 
-  if (slider) {
-    const pagination = slider.querySelector(".swiper-pagination");
-    const btnNext = slider.querySelector(".swiper-button-next");
-    const btnPrev = slider.querySelector(".swiper-button-prev");
+	if (slider) {
+		const pagination = slider.querySelector(".swiper-pagination");
+		const btnNext = slider.querySelector(".swiper-button-next");
+		const btnPrev = slider.querySelector(".swiper-button-prev");
 
-    new window.Swiper(slider, {
-      slidesPerView: 1,
-      spaceBetween: 20,
+		new window.Swiper(slider, {
+			slidesPerView: 1,
+			spaceBetween: 20,
 
-      breakpoints: {
-        580: {
-          slidesPerView: 2,
-        },
+			breakpoints: {
+				580: {
+					slidesPerView: 2,
+				},
 
-        800: {
-          slidesPerView: 3,
-        },
+				800: {
+					slidesPerView: 3,
+				},
 
-        1240: {
-          slidesPerView: 4,
-        },
-      },
+				1240: {
+					slidesPerView: 4,
+				},
+			},
 
-      navigation: {
-        nextEl: btnNext ? btnNext : null,
-        prevEl: btnPrev ? btnPrev : null,
-      },
+			navigation: {
+				nextEl: btnNext ? btnNext : null,
+				prevEl: btnPrev ? btnPrev : null,
+			},
 
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-  }
+			pagination: {
+				el: pagination ? pagination : null,
+				clickable: true,
+			},
+		});
+	}
 });
