0ddff43db84421a344aa8e6c65e48fa3a7233868
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-06-01 14:32:13 +0300

    Настройка поиска в шапке

--- local/templates/rise-bags/header.php
+++ local/templates/rise-bags/header.php
@@ -123,38 +123,38 @@
             if (\Bitrix\Main\ModuleManager::isModuleInstalled('search')):
           ?>
               <? $APPLICATION->IncludeComponent(
-                "bitrix:search.title",
-                "search-title",
-                [
-                  "NUM_CATEGORIES" => "1",
-                  "TOP_COUNT" => "5",
-                  "CHECK_DATES" => "N",
-                  "SHOW_OTHERS" => "N",
-                  "PAGE" => SITE_DIR . "catalog/",
-                  "CATEGORY_0_TITLE" => GetMessage("SEARCH_GOODS"),
-                  "CATEGORY_0" => [
-                    0 => "iblock_catalog",
-                  ],
-                  "CATEGORY_0_iblock_catalog" => [
-                    0 => "all",
-                  ],
-                  "CATEGORY_OTHERS_TITLE" => GetMessage("SEARCH_OTHER"),
-                  "SHOW_INPUT" => "Y",
-                  "INPUT_ID" => "title-search-input",
-                  "CONTAINER_ID" => "search",
-                  "PRICE_CODE" => [
-                    0 => "BASE",
-                  ],
-                  "SHOW_PREVIEW" => "Y",
-                  "PREVIEW_WIDTH" => "75",
-                  "PREVIEW_HEIGHT" => "75",
-                  "CONVERT_CURRENCY" => "Y",
-                  "COMPONENT_TEMPLATE" => "search-title",
-                  "ORDER" => "date",
-                  "USE_LANGUAGE_GUESS" => "Y"
-                ],
-                false
-              ); ?>
+	"bitrix:search.title", 
+	"search-title", 
+	[
+		"NUM_CATEGORIES" => "1",
+		"TOP_COUNT" => "5",
+		"CHECK_DATES" => "Y",
+		"SHOW_OTHERS" => "N",
+		"PAGE" => SITE_DIR."catalog/",
+		"CATEGORY_0_TITLE" => "",
+		"CATEGORY_0" => [
+			0 => "iblock_catalog",
+		],
+		"CATEGORY_0_iblock_catalog" => [
+			0 => "2",
+		],
+		"CATEGORY_OTHERS_TITLE" => GetMessage("SEARCH_OTHER"),
+		"SHOW_INPUT" => "Y",
+		"INPUT_ID" => "title-search-input",
+		"CONTAINER_ID" => "title-search",
+		"PRICE_CODE" => [
+			0 => "BASE",
+		],
+		"SHOW_PREVIEW" => "Y",
+		"PREVIEW_WIDTH" => "75",
+		"PREVIEW_HEIGHT" => "75",
+		"CONVERT_CURRENCY" => "Y",
+		"COMPONENT_TEMPLATE" => "search-title",
+		"ORDER" => "date",
+		"USE_LANGUAGE_GUESS" => "Y"
+	],
+	false
+); ?>
           <?php
             endif;
           endif;
