9bc3b0aebf8cc54ba798adaa7fdf087c3059cb70
OkTarasova <118619788+OkTarasova@users.noreply.github.com>
2026-06-20 17:31:50 +0300

    Правки в result.modifier топ баннера

--- local/templates/rise-bags/components/bitrix/news.list/top-banner/result_modifier.php
+++ local/templates/rise-bags/components/bitrix/news.list/top-banner/result_modifier.php
@@ -3,6 +3,8 @@
 $curPage = $APPLICATION->getCurPage();
 
 if ($curPage == '/') {
+	$arItems = [];
+
   foreach ($arResult["ITEMS"] as $arItem) {
     if ($arItem["PROPERTIES"]["SHOW_ON_INDEX_PAGE"]["VALUE"] == "Y") {
       $arItems[] = $arItem;
